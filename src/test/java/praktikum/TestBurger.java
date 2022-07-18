7d740cc5cd9192baf035a889253e77de3b7ecfbf
Yana <patrina.yana.vadimovna@gmail.com>
2022-07-19 00:56:26 +0300

    diplom-1. "Burger" 21%

--- src/test/java/praktikum/TestBurger.java
+++ src/test/java/praktikum/TestBurger.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.matchers.Null;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
@@ -12,52 +13,40 @@ import java.util.List;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static praktikum.IngredientType.SAUCE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestBurger {
-
-    @Mock
-    //Burger burger;
-    IngredientType type;
-    Ingredient ingredient;
-   // Bun bun;
 
     @Test
     public void testSetBuns(){
         Burger burger = new Burger();
         Bun bun = new Bun("",0);
         burger.setBuns(bun);
-        assertNotNull(bun);
+        assertNotNull(burger.bun);
     }
 
     @Test
     public void testAddIngredient(){
         Burger burger = new Burger();
-        List<Ingredient> ingredients = new ArrayList<>();
-        Ingredient ingredient = new Ingredient(type,"",0);
+        int expected = 1;
+        Ingredient ingredient = new Ingredient(SAUCE,"",0);
         burger.addIngredient(ingredient);
-        //assertEquals(0,ingredients.size());
-        //    public void addIngredient(Ingredient ingredient) {
-        //        ingredients.add(ingredient);
+        assertEquals(expected,burger.ingredients.size());
+    }
+//    public void removeIngredient(int index) {
+//        ingredients.remove(index);
+    @Test
+    public void testRemoveIngredient() {
+        Burger burger = new Burger();
+        Ingredient ingredient = new Ingredient(SAUCE,"",0);
+        int expected = 1;
+        burger.ingredients.add(ingredient);
+        burger.ingredients.add(ingredient);
+        burger.removeIngredient(0);
+        assertEquals(expected,burger.ingredients.size());
     }
 
-   /* @Test
-    public void testRemoveIngredient() {
-        List<Ingredient> ingredients = new ArrayList<>();
-        Ingredient ingredient = new Ingredient(type,"",0);
-        int expected = 2;
-        ingredients.add(ingredient);
-        ingredients.add(ingredient);
-        ingredients.add(ingredient);
-        System.out.println(ingredients.size());
-
-       // burger.removeIngredient(0);
-        System.out.println(ingredients.size());
-
-        ingredients.remove(0);
-        System.out.println(ingredients.size());
-        //assertEquals(expected,ingredients.size());
-    }*/
 }
 //    public Bun bun;
 //    public List<Ingredient> ingredients = new ArrayList<>();
